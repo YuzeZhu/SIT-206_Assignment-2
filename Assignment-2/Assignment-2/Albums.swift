0b066cfeda14a1c4915bae5527cfd697b087aaf2
ZHU YUZE ZHU <zhuyuz@l-b-v239-03.local>
2018-04-22 19:25:13 +1000

    2nd

---
+++ Assignment-2/Assignment-2/Albums.swift
@@ -0,0 +1,35 @@
+//
+//  Albums.swift
+//  Assignment-2
+//
+//  Created by ZHU YUZE ZHU on 22/4/18.
+//  Copyright © 2018 ZHU YUZE ZHU. All rights reserved.
+//
+
+import UIKit
+
+class Albums: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
