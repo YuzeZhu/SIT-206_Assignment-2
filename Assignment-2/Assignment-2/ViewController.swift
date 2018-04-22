268b6b25905e68591f1c3418bf8d625e5fa46198
ZHU YUZE ZHU <zhuyuz@10-151-189-206.dhcp-m.deakin.edu.au>
2018-04-22 16:42:23 +1000

    1st try

---
+++ Assignment-2/Assignment-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment-2
+//
+//  Created by ZHU YUZE ZHU on 22/4/18.
+//  Copyright © 2018 ZHU YUZE ZHU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
